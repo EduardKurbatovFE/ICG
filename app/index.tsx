e94994f346844bd12871433578d1342589cd444a
EduardKurbatov <eduard.kurbatov4130@gmail.com>
2025-06-24 01:12:39 +0300

    removed default assets

--- app/index.tsx
+++ app/index.tsx
@@ -9,7 +9,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>
+        Cлевін, я законфіжив на двої девайсах все!
+      </Text>
     </View>
   );
 }
